022fb24c349b5eddf334bb59c3453fc92ea5308b
d4rki3 <jarson.santos@gmail.com>
2019-05-16 15:49:37 -0300

    Create script.js

---
+++ js/script.js
@@ -0,0 +1,18 @@
+var c = document.getElementById("myCanvas");
+		var ctx = c.getContext("2d");
+		
+		ctx.font = "30px Verdana";
+		// Create gradient
+		var gradient = ctx.createLinearGradient(0, 0, c.width, 0);
+		gradient.addColorStop("0", "green");
+		gradient.addColorStop("0.5", "blue");
+		gradient.addColorStop("1.0", "red");
+		// Fill with gradient
+		ctx.fillStyle = gradient;
+		ctx.fillText("Desenvolvedor G33k", 10, 80);
+		
+		ctx.font = "20px Georgia";
+		ctx.fillText("+55 12 996003513", 55, 115);
+		
+		ctx.font = "20px Georgia";
+		ctx.fillText("D4rki3 - Jarson Santos", 45, 40);
