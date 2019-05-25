ec9afdb0778d7e7d203c42cc34bf1cfe1b4175d4
d4rki3 <jarson.santos@gmail.com>
2019-05-25 12:48:14 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -9,7 +9,7 @@ gradient.addColorStop("0.5", "blue");
 gradient.addColorStop("1.0", "red");
 // Fill with gradient
 ctx.fillStyle = gradient;
-ctx.fillText("Desenvolvedor G33k", 0, 80);
+ctx.fillText("Desenvolvedor G33k", 10, 80);
 
 ctx.font = "20px Georgia";
 ctx.fillText("+55 12 996003513", 55, 115);
