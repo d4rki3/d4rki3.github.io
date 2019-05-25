ec7271d91408b678fcf9a5eef6cc16ba978f6efd
d4rki3 <jarson.santos@gmail.com>
2019-05-25 18:11:50 -0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,7 +1,8 @@
 var c = document.getElementById("myCanvas");
 var ctx = c.getContext("2d");
 
-ctx.font = "27px Verdana";
+ctx.textAlign = "center"
+ctx.font = "25px Verdana";
 // Create gradient
 var gradient = ctx.createLinearGradient(0, 0, c.width, 0);
 gradient.addColorStop("0", "green");
@@ -9,10 +10,10 @@ gradient.addColorStop("0.5", "blue");
 gradient.addColorStop("1.0", "red");
 // Fill with gradient
 ctx.fillStyle = gradient;
-ctx.fillText("Desenvolvedor G33k", 10, 80);
+ctx.fillText("Desenvolvedor G33k", 150, 80);//10, 80);
 
 ctx.font = "20px Georgia";
-ctx.fillText("+55 12 996003513", 55, 115);
+ctx.fillText("+55 12 996003513", 150, 115);//55, 115);
 
-ctx.font = "20px Georgia";
-ctx.fillText("D4rki3 - Jarson Santos", 45, 40);
+//ctx.font = "20px Georgia";
+ctx.fillText("D4rki3 - Jarson Santos", 150, 40);//45, 40);
